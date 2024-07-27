fd135f244f534de8c42e1d5162d81950400c97f7
Asmita Adhikari <ushmeadhikari@gmail.com>
2024-07-27 20:00:39 +1000

    newblogtry

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Laravel Blog</title>
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+</head>
+<body>
+    <div class="container">
+        @yield('content')
+    </div>
+</body>
+</html>
